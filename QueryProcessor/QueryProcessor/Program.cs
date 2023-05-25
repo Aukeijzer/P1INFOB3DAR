01c33ee7ffb2f94144d3bfe5ecf846469f4a083f
AukeIjzer <auke.roest01@gmail.com>
2023-05-25 16:37:29 +0200

    parser and packages

---
+++ QueryProcessor/QueryProcessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using QueryProcessor.Properties;
+
+namespace QueryProcessor
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Interface.Run();
+        }
+    }
+}
